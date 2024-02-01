ebc3bd99946496adbfb2bc5d10564342abfffd18
André Ferreira <andreferreiramusic@gmail.com>
2024-02-01 15:54:22 +0100

    commit2 M4-C1

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-let result;
 
 
-function dueNumeriInteri (num1,num2) {
-    if ((num1 === 50) || (num2 === 50) || ((num1+num2)===50)) {
-        console.log("true");
-        return true; 
-    }else {
-        console.log("false");
-    }
-}
-dueNumeriInteri(50,20);
+
+
+// Exercise 1 
+
+// function dueNumeriInteri (num1,num2) {
+//     if ((num1 === 50) || (num2 === 50) || ((num1+num2)===50)) {
+//         console.log("true");
+//         return true; 
+//     }else {
+//         console.log("false");
+//     }
+// }
+// dueNumeriInteri(50,20);
+
+
+//Exercise 2 
+
+// let myString = "Hello everyone! How are you?";
+// let charPosition = 2;
+// function removeChar (myString, charPosition ) {
+//     let newString = myString.slice(0,charPosition) + myString.slice(charPosition+1);
+//     console.log(newString);
+// }
+
+// removeChar(myString, 7);
+
+
+
+//Exercise 3 
+
+// function dueNumeri (num1,num2) {
+//     if (((num1 > 40) & (num2 > 40)) & ((num1 < 60) & (num2 < 60))   || ((num1 > 70) & (num2 > 70)) & ((num1 < 100) & (num2 < 100))) {
+//         console.log("true");
+//         return true; 
+//     }else {
+//         console.log("false");
+//     }
+// }
+// dueNumeri(42,72);
+
 
