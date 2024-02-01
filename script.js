1f7dbeafe9068990fd87f5952240114129b99b76
André Ferreira <andreferreiramusic@gmail.com>
2024-02-01 14:23:45 +0100

    M3-C1

---
+++ script.js
@@ -0,0 +1,13 @@
+let result;
+
+
+function dueNumeriInteri (num1,num2) {
+    if ((num1 === 50) || (num2 === 50) || ((num1+num2)===50)) {
+        console.log("true");
+        return true; 
+    }else {
+        console.log("false");
+    }
+}
+dueNumeriInteri(50,20);
+
